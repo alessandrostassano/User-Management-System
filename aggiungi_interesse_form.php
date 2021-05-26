b4bf09a01d1805241dba915a0dacd54af9bd97aa
alessandrostassano <alestassa@gmail.com>
2021-05-26 12:44:29 +0200

    tasto a caso (aggiungi int) aggiunto ma eliminato

--- aggiungi_interesse_form.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-
-include './add_user_form.php';
-include './src/view/add_user_view.php';
--- src/view/add_interest_view.php
+++
@@ -1,5 +0,0 @@
-<?php ?>
-
-
-<button class="btn btn-primary mt-3" type="submit"><?= $submit ?></button>
-<button class="btn btn-primary mt-3" type="submit"><?= $submit ?></button>
